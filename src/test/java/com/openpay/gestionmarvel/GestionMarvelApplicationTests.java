b373d92de68ab78f8ff7dcab491fcc8b54f88a8b
Benjamin Mac <benjamin94833@gmail.com>
2024-03-24 18:21:39 -0600

    se realiza commit inicial de proyecto

---
+++ src/test/java/com/openpay/gestionmarvel/GestionMarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpay.gestionmarvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
